f8d520e8233d44f0bbc9fea95adf538305502d9c
OjoPublico <giancohs@gmail.com>
2023-05-26 09:21:17 -0500

    updating scripts and plotting

--- scripts/01-process.R
+++ scripts/01-process.R
@@ -1,13 +1,8 @@
 library(tidyverse)
 library(lubridate)
-
-# Setear directorio de trabajo con path relativo
-setwd("../")
+library(googlesheets4)
 
 # Importar datos
-comercio_paiche <- read_csv("data/comptab_2023-05-25 18 30_comma_separated.csv")
-
-comercio_paiche_clean <- comercio_paiche %>% 
-    mutate(
-        case_when()
-    )
+comercio_paiche <- read_sheet(
+    "https://docs.google.com/spreadsheets/d/1WDqeBOaxr1jOkbBlRbsGmGGAj3YQTghRShyBn8aJEP4/edit#gid=0"
+)
